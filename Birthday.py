68ac6eefc8b9370682e3ecf8d002cafcb8ca33a1
Jagadish2019 <jagadishg2019@gmail.com>
2019-09-25 22:23:20 +0530

    second commit of basic program

---
+++ Birthday.py
@@ -0,0 +1,8 @@
+# Next birthday of yours
+
+name = input("Enter the name: ")
+age = int(input("Enter the age: "))
+newAge = age + 1
+
+print(name,"next birthday you will be ",newAge)
+
---
+++ KiloToPounds.py
@@ -0,0 +1,7 @@
+# converting Kilo to Pounds
+
+kilo = int(input("Enter the number of kilos: "))
+
+pound = kilo * 2.204
+
+print(kilo,"kilo is ", pound," pounds")
