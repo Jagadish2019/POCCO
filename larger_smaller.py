80aca494581ce9dcd2066e2c44c15c33c0749c48
Jagadish2019 <jagadishg2019@gmail.com>
2019-09-25 22:44:32 +0530

    Create larger_smaller.py
    number_exercise

---
+++ larger_smaller.py
@@ -0,0 +1,8 @@
+#smaller goes into larger  how many times
+
+larger = int(input("Enter a number over 100: "))
+smaller = int(input("Enter a number under 10: "))
+
+answer = larger // smaller
+
+print(smaller," goes into ", larger , ",",answer ," times")
