a444d8fef8674e58efd904ce584e849e98f739db
Jagadish2019 <jagadishg2019@gmail.com>
2019-09-25 22:20:32 +0530

    first commit of basic program

---
+++ Bill_Share.py
@@ -0,0 +1,8 @@
+# The bill will be shared between the people
+
+Total = int(input("Enter the total amount: "))
+People = int(input("Enter the number of people who gonna share the total amount: "))
+
+each = Total/People
+
+print("Each person should pay Rs. ", each)
